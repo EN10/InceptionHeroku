ba840892ab095c3f7288f0cd63afe730964fb4e0
Enio Fernandes <ef@cic.ac>
2018-01-22 10:38:17 +0000

    None

--- app.py
+++ app.py
@@ -7,8 +7,11 @@ import subprocess
 def index():
     
     q = request.args.get('q')
-    p = subprocess.Popen(["bash", "run.sh", q], stdout=subprocess.PIPE)
-    output, err = p.communicate()
-    output = output.replace('\n','<br>')
+    if q <> None:
+        p = subprocess.Popen(['bash', 'run.sh', q], stdout=subprocess.PIPE)
+        output, err = p.communicate()
+        output = output.replace('\n','<br>')
+    else:
+        output = 'ERROR: No URL Given!'
 
     return output
